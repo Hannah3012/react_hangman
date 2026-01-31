918af58dfbcd33f83396dc2e0fe27172bdf76933
Hanna Tariku <hannatariku1996@gmail.com>
2026-01-31 14:31:15 +0300

    chore: add word list JSON and initial app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
 function App() {
-  return <h1>Hi</h1>
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]; // returns a random word
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]> ([])
+
+  
+  return <h1>Hi</h1>;
 }
 
-export default App
+export default App;
