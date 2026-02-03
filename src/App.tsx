020497782e204f0303b0576d81942978c1730d53
Hanna Tariku <hannatariku1996@gmail.com>
2026-02-03 14:23:13 +0300

    Add initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,22 @@ function App() {
     return words[Math.floor(Math.random() * words.length)]; // returns a random word
   });
 
-  const [guessedLetters, setGuessedLetters] = useState<string[]> ([])
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
-  
-  return <h1>Hi</h1>;
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}> Lose Win </div>
+    </div>
+  );
 }
 
 export default App;
